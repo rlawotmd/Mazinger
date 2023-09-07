1f1b39dc9c69e536d4db5e5ec31dfd0e3c0923d7
rlawotmd <kimjs2400@naver.com>
2023-09-07 13:50:23 +0900

    7-9

--- src/Main.java
+++ src/Main.java
@@ -137,6 +137,44 @@ class MyPanel extends JPanel {
     }
     */
 
+    /* 7-9
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int r = 40;
+        int cx = 50;
+        int cy = 100;
+        int angle = 30;
+
+        int dx = (int)(r * Math.cos(angle * Math.PI / 180));
+        int dy = (int)(r * Math.sin(angle * Math.PI / 180));
+
+        g.fillArc(cx - r, cy - r, 2 * r, 2 * r, angle, 360 - 2 * angle);
+
+        Polygon p = new Polygon();
+        cx = 150;
+        int i;
+        for(i = 0; i < 5; i++)
+            p.addPoint(
+                    (int)(cx + r * Math.cos(i * 2 * Math.PI / 5)),
+                    (int)(cy + r * Math.sin(i * 2 * Math.PI / 5))
+            );
+
+        g.fillPolygon(p);
+
+        Polygon s = new Polygon();
+        cx = 250;
+        for(i = 0; i < 360; i++) {
+            double t = i / 360.0;
+            s.addPoint(
+                    (int)(cx + r * t * Math.cos(8 * t * Math.PI)),
+                    (int)(cy + r * t * Math.sin(8 * t * Math.PI))
+            );
+        }
+        g.fillPolygon(s);
+    }
+    */
+
+
 }
 
 class MyFrame extends JFrame {
